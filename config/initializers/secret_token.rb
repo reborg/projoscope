a67f80b096c124cac7485ebdb71a3ec4cf9b6d90
reborg <reborg@reborg.net>
2010-10-13 12:23:02 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projoscope::Application.config.secret_token = '8ae64e66d0d8d9ece93b68c59e82ca31ae36d0c18b23b47f1b7b0315d02ca56fa79d4fc86bd2bba52c52d881c2db0bc0cd165769a1828cf3521586f2265bc66e'
