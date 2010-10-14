5d88e614201008ab05235be106e84abf7109dc8c
reborg <reborg@reborg.net>
2010-10-14 19:56:53 +0100

    Flash message should disappear at the end of the current action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
     error = "Project ' #{params[:project]} ' does not exist in the issue tracking system"
     flash[:error] = error unless @project = IssueTracking.new.project_by_name(params[:project])
     render :template => 'home/chart' 
+    flash.discard
   end
 
 end
